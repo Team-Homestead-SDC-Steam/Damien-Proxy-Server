a2d07d796d9bda5d1415cdd6e27309abdb50dbb4
Damien Jones <damienjones@gmail.com>
2020-07-31 16:02:03 -0700

    adding tag service to URL list

--- URLs.js
+++ URLs.js
@@ -3,7 +3,7 @@ const dev = false;
 const URLs = [
   {
     match: '/api/gamereviews/', 
-    server: 'ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3001',
+    server: 'ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3000',
   },
   {
     match: '/api/reviewcount/',
@@ -20,6 +20,10 @@ const URLs = [
   {
     match: '/api/description/',
     server: 'ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3005',
+  },
+  {
+    match: '/api/tags/',
+    server: '44.233.13.178:3006',
   },
   {
     match: '/tags.css',
